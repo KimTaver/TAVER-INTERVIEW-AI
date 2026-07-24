e24353021855371924a029ded5a4c4f2f7480ed5
KimTaver <kakashi4q@gmail.com>
2026-07-24 15:06:28 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,51 +1,47 @@
 const questions = require("../handlers/questions");
 const interviewManager = require("../handlers/interviewManager");
+const transcript = require("../handlers/transcript");
 
 module.exports = {
   name: "messageCreate",
 
   async execute(message) {
-
     if (message.author.bot) return;
 
     if (!message.channel.name.startsWith("interview-")) return;
 
     if (!interviewManager.hasInterview(message.author.id)) return;
 
-    const nextQuestion =
-      interviewManager.saveAnswer(
-        message.author.id,
-        message.content
-      );
+    const nextQuestion = interviewManager.saveAnswer(
+      message.author.id,
+      message.content
+    );
 
     if (!nextQuestion) {
+      const data = interviewManager.finishInterview(
+        message.author.id
+      );
 
-      const data =
-        interviewManager.finishInterview(
-          message.author.id
-        );
+      transcript.saveTranscript(
+        message.author,
+        data
+      );
 
       await message.channel.send(
         "✅ Your interview has been completed.\nThank you for your time!"
       );
 
-      console.log(data);
-
       return;
     }
 
-    const interview =
-      data = interviewManager.hasInterview(message.author.id);
+    const current = interviewManager.getQuestion(
+      message.author.id
+    );
 
-    const current =
-      interviewManager.getQuestion(message.author.id);
-
-    const index =
-      questions.indexOf(current) + 1;
+    const index = questions.indexOf(current) + 1;
 
     await message.channel.send(
       `**Question ${index}/${questions.length}**\n\n${nextQuestion}`
     );
-
   },
 };
