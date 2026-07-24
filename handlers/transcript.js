54975c71450d08097aa7f3a5b5045f660f9769be
KimTaver <kakashi4q@gmail.com>
2026-07-24 15:04:03 +0100

    Update transcript.js

--- handlers/transcript.js
+++ handlers/transcript.js
@@ -1,1 +1,34 @@
-i
+const fs = require("fs");
+const path = require("path");
+
+const FILE = path.join(
+  __dirname,
+  "../data/interviews.json"
+);
+
+function saveTranscript(user, interview) {
+
+  let data = [];
+
+  if (fs.existsSync(FILE)) {
+    data = JSON.parse(
+      fs.readFileSync(FILE, "utf8")
+    );
+  }
+
+  data.push({
+    user: user.tag,
+    id: user.id,
+    date: new Date().toISOString(),
+    answers: interview.answers,
+  });
+
+  fs.writeFileSync(
+    FILE,
+    JSON.stringify(data, null, 2)
+  );
+}
+
+module.exports = {
+  saveTranscript,
+};
