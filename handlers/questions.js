83bfaeb2a3e664170416a51d50a5b9618417ee03
KimTaver <kakashi4q@gmail.com>
2026-07-24 14:41:18 +0100

    Update questions.js

--- handlers/questions.js
+++ handlers/questions.js
@@ -1,1 +1,12 @@
-j
+module.exports = [
+  "👋 Welcome! Let's begin.\n\nWhat is your full Discord username?",
+  "How old are you?",
+  "What country are you from?",
+  "Why do you want to join our organization?",
+  "Do you have any previous experience in similar roles?",
+  "How active can you be every week?",
+  "How would you handle a difficult member?",
+  "What are your strengths?",
+  "What are your weaknesses?",
+  "Is there anything else you'd like us to know before we finish?"
+];
