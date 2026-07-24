8bbbbf12249d27080f9897d21ff96d7c4c62bf8e
KimTaver <kakashi4q@gmail.com>
2026-07-24 12:33:11 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,48 @@
-.
+const {
+  REST,
+  Routes,
+} = require("discord.js");
+
+require("dotenv").config();
+
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  name: "ready",
+  once: true,
+
+  async execute(client) {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+
+    const commands = [];
+
+    const commandFiles = fs
+      .readdirSync(path.join(__dirname, "../commands"))
+      .filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+      const command = require(`../commands/${file}`);
+      commands.push(command.data.toJSON());
+    }
+
+    const rest = new REST({
+      version: "10",
+    }).setToken(process.env.TOKEN);
+
+    try {
+      console.log("🔄 Registering slash commands...");
+
+      await rest.put(
+        Routes.applicationCommands(process.env.CLIENT_ID),
+        {
+          body: commands,
+        }
+      );
+
+      console.log("✅ Slash commands registered.");
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
