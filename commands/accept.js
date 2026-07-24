dba25aec7adbd89d8e5487024a49d5fa1965a46c
KimTaver <kakashi4q@gmail.com>
2026-07-24 15:20:26 +0100

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -1,1 +1,18 @@
-.
+const {
+  SlashCommandBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("accept")
+    .setDescription("Accept an interview applicant"),
+
+  async execute(interaction) {
+
+    await interaction.reply({
+      content: "✅ Applicant accepted.",
+      ephemeral: true,
+    });
+
+  },
+};
