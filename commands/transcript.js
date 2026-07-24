a02a304a17bf6b95d23d39122b257af510b1e007
KimTaver <kakashi4q@gmail.com>
2026-07-24 15:21:16 +0100

    Update transcript.js

--- commands/transcript.js
+++ commands/transcript.js
@@ -1,1 +1,19 @@
-.
+const {
+  SlashCommandBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("transcript")
+    .setDescription("View interview transcripts"),
+
+  async execute(interaction) {
+
+    await interaction.reply({
+      content:
+        "📄 Transcript system coming soon.",
+      ephemeral: true,
+    });
+
+  },
+};
