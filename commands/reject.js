932578fd5cc4f043495df93cd6a8d1b039d06658
KimTaver <kakashi4q@gmail.com>
2026-07-24 15:20:48 +0100

    Update reject.js

--- commands/reject.js
+++ commands/reject.js
@@ -1,1 +1,18 @@
-.
+const {
+  SlashCommandBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("reject")
+    .setDescription("Reject an interview applicant"),
+
+  async execute(interaction) {
+
+    await interaction.reply({
+      content: "❌ Applicant rejected.",
+      ephemeral: true,
+    });
+
+  },
+};
